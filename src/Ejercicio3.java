f682406a5d931141d94b19b9b310700f13976545
YerayJ <yjimenez@iesnervion.es>
2022-11-22 12:56:04 +0100

    Ejercicio Terminado

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -16,19 +16,21 @@ public class Ejercicio3 {
                 double area = 2 * radio * (altura + radio);
                 // Imprimimos el area con solo dos decimales
                 System.out.printf("El área es %.2f", area);
-                // 
+                // Usamos el break para salir directamente del programa
                 break;
             case 2:
+                // Indicamos volumen de tipo double para sacar decimales y metemos la fórmula
+                // dentro de ella. Usamos la función Math.PI para sacar el número PI
                 double volumen = Math.PI * (radio * radio) * altura;
-                 // Imprimimos el volumen con solo dos decimales
+                // Imprimimos el volumen con solo dos decimales
                 System.out.printf("El volumen es %.2f", volumen);
+                // Usamos el break para salir directamente del programa
                 break;
-
+            // Para el default le diremos que la opción pasada no es correcta
             default:
                 System.out.println("La opción pasada no es correcta");
                 break;
         }
-
     }
 
     public static void main(String[] args) {
